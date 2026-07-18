0333864f50bb6f250259d0e4e7fd2d74d70eb91b
tc0512 <tancheng_0812@qq.com>
2026-07-18 22:39:26 +0800

    Add the input function

--- io.py
+++ io.py
@@ -8,3 +8,32 @@ def INFO_to_print(code: str):
         raise SyntaxError(f"Expected `[INFO]` got `{keyword}`")
     text = text.removeprefix("[").removesuffix("]")
     return f"print({text})"
+
+# [INPUT] [<tip word>] [<multilines>] → input(<tip word>)
+def INPUT_to_input(code: str):
+    keyword = code.split(" ", 1)[0]
+    if keyword != "[INPUT]":
+        raise SyntaxError(f"Expected `[INPUT]` got `{keyword}`")
+    rest = code.split(" ", 1)[1]
+    inner = rest.removeprefix("[").removesuffix("]")
+    parts = inner.split("][")
+    if len(parts) != 2:
+        raise SyntaxError("Usage: [INPUT] [<tip word>] [<multilines> (bool)]")
+    tip_word = parts[0]
+    multilines = parts[1].lower() == "true"
+    if multilines:
+        return f'''\
+print("{tip_word} (use EOF to finish)")
+lines = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    lines.append(line)
+user_input = "\\n".join(lines)
+'''
+    else:
+        return f'''\
+user_input = input("{tip_word}: ")
+'''
