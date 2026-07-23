7d5ddfeb8cf33f3c6f374091bdb96cfa99786d47
tc0512 <tancheng_0812@qq.com>
2026-07-23 21:21:30 +0800

    Add importlib

---
+++ IMPORTLIB.py
@@ -0,0 +1,14 @@
+# bracket/IMPORTLIB.py
+# manage libraries importing
+
+# [USE] [<lib>] → import <lib>
+def USE_to_import(code: str):
+    code = code.lstrip()
+    keyword, lib = code.split(" ", 1)
+    if keyword!="[USE]":
+        raise SyntaxError(f"Expect `[USE]` got {keyword}")
+    key = cond.removeprefix("[").removesuffix("]")
+    lib_dict = {
+        "GUI": "tkinter as GUI"
+    }
+    return f"import {lib_dict[key]}"
