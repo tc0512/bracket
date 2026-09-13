93cdf68b6f9dbed78ea6757ab134ed347a835b59
tc0512 <tancheng_0812@qq.com>
2026-09-13 14:04:41 +0500

    Update IMPORTLIB.py

--- bracket/IMPORTLIB.py
+++ bracket/IMPORTLIB.py
@@ -9,7 +9,6 @@ def USE_to_import(code: str):
         raise SyntaxError(f"Expect `[USE]` got {keyword}")
     key = lib.removeprefix("[").removesuffix("]")
     lib_dict = {
-        "TKGUI": "tkinter as TKGUI",
-        "PACKAGING": "PyInstaller.__main__\nPACK = PyInstaller.__main__.run"
+        "GUI": "bracket.GUI",
     }
-    return f"import {lib_dict[key]}"
+    return f"from {lib_dict[key]} import*"
