02700b9c96b51f5a10c04812de1f281790da18dd
tc0512 <tancheng_0812@qq.com>
2026-09-13 14:03:35 +0500

    Integration GUI functions into bracket-lang itself.

---
+++ bracket/GUI.py
@@ -0,0 +1,16 @@
+# bracket/GUI.py
+# GUI support for bracket
+
+import tkinter as tk
+
+Base = tk.Tk
+Tag = tk.Label
+Button = tk.Button
+InputBox = tk.Entry
+Text = tk.Text
+Checkbutton = tk.Checkbutton
+Radiobutton = tk.Radiobutton
+Int = tk.IntVar
+String = tk.StringVar
+Double = tk.DoubleVar
+Bool = tk.BooleanVar
